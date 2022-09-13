8dcb3bcb509e36a65d9ccdbb6dfbdf0cbe619270
polarr <Charles.Ran9@gmail.com>
2022-09-12 22:30:22 -0400

    feat: realtime public rooms subscription

--- index.ts
+++ index.ts
@@ -20,6 +20,14 @@ const supabase = createClient(
     process.env.SUPABASE_ANON_KEY
 );
 
+let { data: publicRooms, error } = await supabase.from('rooms').select(`id, name, description, mode, players`, { count: 'exact' }).eq('public', 'true');
+console.log(publicRooms);
+
+const publicRoomsSubscription = supabase.from('rooms').on('*', async payload => {
+    console.log(payload);
+    let { data, error } = await supabase.from('rooms').select(`id, name, description, mode, players`, { count: 'exact' }).eq('public', 'true');
+    publicRooms = data;
+}).subscribe();
 
 io.on("connection", (socket: Socket) => {
     console.log(socket.id);
