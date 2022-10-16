dd7cd942b9cb6b857b6855cc9e2c9dc35861f6d2
polarr <Charles.Ran9@gmail.com>
2022-10-15 21:29:22 -0400

    feat: room creation

--- index.ts
+++ index.ts
@@ -9,7 +9,7 @@ import { Server, Socket } from "socket.io";
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.NODE_ENV === "development" ? "*" : "https://voly.mathetal.org",
     },
 });
 
@@ -17,20 +17,48 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
     process.env.SUPABASE_URL,
-    process.env.SUPABASE_ANON_KEY
+    process.env.SUPABASE_SERVICE_KEY
 );
 
-let { data: publicRooms, error } = await supabase.from('rooms').select(`id, name, description, mode, players`, { count: 'exact' }).eq('public', 'true');
+let { data: publicRooms, error, status } = await supabase.from('rooms').select(`id, name, description, mode, players`, { count: 'exact' }).eq('public', 'true');
 console.log(publicRooms);
 
-const publicRoomsSubscription = supabase.from('rooms').on('*', async payload => {
-    console.log(payload);
+supabase.channel('public:rooms').on('postgres_changes', {event: '*', schema: '*', table:'rooms'}, async payload => {
     let { data, error } = await supabase.from('rooms').select(`id, name, description, mode, players`, { count: 'exact' }).eq('public', 'true');
     publicRooms = data;
+    console.log(publicRooms);
 }).subscribe();
 
 io.on("connection", (socket: Socket) => {
     console.log(socket.id);
+
+    socket.on("create-room", async ({ user, data: { name, description, mode, isPublic }}) => {
+        try {
+            const { data, error } = await supabase.from('rooms').upsert([{
+                name,
+                description,
+                mode,
+                isPublic,
+                players: JSON.stringify({
+                    [socket.id]: {
+                        user, 
+                        answers: []
+                    }
+                }),
+                created_at: new Date()
+            }]).select();
+    
+            if (error) {
+                throw error;
+            }
+
+            socket.emit('create-room-success', {data});
+        }
+        catch ({message}) {
+            socket.emit("create-room-error", { data: "Error Creating Room: " + message });
+            console.error("Error Creating Room:", message);
+        }
+    });
 });
 
 httpServer.listen(4000);
