489270e525b0a0425a90e1f4f95b41d120228443
polarr <Charles.Ran9@gmail.com>
2022-11-21 22:45:36 -0500

    feat: room api initialization on creation and socket disconnect cleanup

--- index.ts
+++ index.ts
@@ -20,26 +20,85 @@ io.on("connection", (socket: Socket) => {
 
     socket.on("create-room", async ({ idToken, data }) => {
         const decoded = await auth.verifyIdToken(idToken);
+        const { uid } = decoded;
         const { roomName: name, roomDescription: description, roomMode: mode, roomPublic } = data;
-        console.log(decoded);
-        console.log(data);
 
         try {
-            await rtdb.ref('rooms/test').set({
+            const rtdbUser = (await rtdb.ref('authUsers/' + uid).once('value')).val();
+
+            if (rtdbUser != null){
+                socket.emit('create-room-error', { error: 'error: room creation: already in a room', message: 'You are already in a room!' });
+                return;
+            }
+
+            console.log(rtdbUser);
+
+            const roomPush = rtdb.ref('rooms').push();
+            const roomKey = roomPush.key;
+
+            await roomPush.set({
                 name,
                 description,
                 mode, 
                 roomPublic,
                 teamsEnabled: false,
                 maxUsers: 8,
-                users: [{
-                    socketId: socket.id,
-                    userId: decoded.uid
-                }]
+                users: {
+                    [socket.id]: {
+                        userId: uid
+                    }
+                }
             });
+
+            await rtdb.ref('roomUsers/' + roomKey).set({
+                responses: {
+                    [uid]: {
+                        socketId: socket.id,
+                        status: 0,
+                        response: null
+                    }
+                }
+            });
+
+            await rtdb.ref('authUsers/' + uid).set({
+                room: roomKey,
+                socketId: socket.id
+            })
+
+            socket.join(roomKey);
         }
         catch (error) {
             console.error(error);
+        }
+    });
+
+    socket.on("disconnecting", async (reason) => {
+        for (const room of socket.rooms){
+            if (room === socket.id){
+                continue;
+            }
+
+            try {
+                const users = (await rtdb.ref('rooms/' + room + '/users').once('value')).val();
+                const { [socket.id]: { userId }} = users;
+
+                if (Object.keys(users).length <= 1){
+                    await Promise.all([
+                        rtdb.ref('rooms/' + room).remove(),
+                        rtdb.ref('roomUsers/' + room).remove(),
+                        rtdb.ref('authUsers/' + userId).remove()
+                    ]);
+                    return;
+                }
+
+                await Promise.all([
+                    rtdb.ref('rooms/' + room + '/users/' + socket.id).remove(),
+                    rtdb.ref('roomUsers/' + room + '/responses/' + userId).remove(),
+                    rtdb.ref('authUsers/' + userId).remove()
+                ]);
+            } catch (error) {
+                console.error(error);
+            }
         }
     });
 });
