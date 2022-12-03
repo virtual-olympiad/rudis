9831d1eb16bdd8c410463ccc36c12a6ce1460e83
polarr <Charles.Ran9@gmail.com>
2022-12-03 17:55:35 -0500

    feat: add room creation optimizations and room joining

--- index.ts
+++ index.ts
@@ -16,13 +16,17 @@ const io = new Server(httpServer, {
 import { app, auth, rtdb, db } from "./firebase.js";
 
 io.on("connection", (socket: Socket) => {
-    console.log(socket.id);
+    console.log(socket.id + " CONNECTS");
 
     socket.on("create-room", async ({ idToken, data }) => {
         const decoded = await auth.verifyIdToken(idToken);
         const { uid } = decoded;
         const { roomName: name, roomDescription: description, roomMode: mode, roomPublic } = data;
 
+        if (uid == null){
+            socket.emit('create-room-error', { error: 'error: room creation: not logged in', message: 'You must log in to create or join rooms!' });
+        }
+        
         try {
             const rtdbUser = (await rtdb.ref('authUsers/' + uid).once('value')).val();
 
@@ -31,41 +35,49 @@ io.on("connection", (socket: Socket) => {
                 return;
             }
 
-            console.log(rtdbUser);
+            console.log(socket.id + " UID:" + uid + " requests CREATE");
 
             const roomPush = rtdb.ref('rooms').push();
-            const roomKey = roomPush.key;
+            const roomId = roomPush.key;
 
-            await roomPush.set({
-                name,
-                description,
-                mode, 
-                roomPublic,
-                teamsEnabled: false,
-                maxUsers: 8,
-                users: {
-                    [socket.id]: {
-                        userId: uid
-                    }
-                }
-            });
-
-            await rtdb.ref('roomUsers/' + roomKey).set({
-                responses: {
-                    [uid]: {
+            await Promise.all([
+                roomPush.set({
+                    name,
+                    description,
+                    mode, 
+                    roomPublic,
+                    teamsEnabled: false,
+                    maxUsers: 8,
+                    host: {
                         socketId: socket.id,
-                        status: 0,
-                        response: null
+                        userId: uid
+                    }, 
+                    users: {
+                        [socket.id]: {
+                            userId: uid
+                        }
                     }
-                }
-            });
+                }),
+                rtdb.ref('roomUsers/' + roomId).set({
+                    responses: {
+                        [uid]: {
+                            socketId: socket.id,
+                            status: 0,
+                            response: null
+                        }
+                    }
+                }),
+                rtdb.ref('publicRooms/' + roomId).set(true),
+                rtdb.ref('authUsers/' + uid).set({
+                    room: roomId,
+                    socketId: socket.id
+                })
+            ]);
 
-            await rtdb.ref('authUsers/' + uid).set({
-                room: roomKey,
-                socketId: socket.id
+            socket.join(roomId);
+            socket.emit('create-room-success', {
+                roomId
             })
-
-            socket.join(roomKey);
         }
         catch (error) {
             console.error(error);
@@ -79,15 +91,21 @@ io.on("connection", (socket: Socket) => {
             }
 
             try {
-                const users = (await rtdb.ref('rooms/' + room + '/users').once('value')).val();
+                const { roomPublic, users }= (await rtdb.ref('rooms/' + room).once('value')).val();
                 const { [socket.id]: { userId }} = users;
 
                 if (Object.keys(users).length <= 1){
-                    await Promise.all([
+                    let deletePromise = [
                         rtdb.ref('rooms/' + room).remove(),
                         rtdb.ref('roomUsers/' + room).remove(),
                         rtdb.ref('authUsers/' + userId).remove()
-                    ]);
+                    ];
+
+                    if (roomPublic){
+                        deletePromise.push(rtdb.ref('publicRooms/' + room).remove());
+                    }
+
+                    await Promise.all(deletePromise);
                     return;
                 }
 
@@ -99,6 +117,62 @@ io.on("connection", (socket: Socket) => {
             } catch (error) {
                 console.error(error);
             }
+        }
+    });
+
+    socket.on("join-room", async ({ idToken, data }) => {
+        const decoded = await auth.verifyIdToken(idToken);
+        const { uid } = decoded;
+        const { code } = data;
+
+        try {
+            const rtdbUser = (await rtdb.ref('authUsers/' + uid).once('value')).val();
+
+            if (rtdbUser != null){
+                socket.emit('join-room-error', { error: 'error: joining room: already in a room', message: 'You are already in a room!' });
+                return;
+            }
+
+            const requestRoom = (await rtdb.ref('rooms/' + code).once('value'));
+
+            if (!requestRoom.exists()){
+                socket.emit('join-room-error', { error: 'error: joining room: room does not exist', message: 'The room does not exist!' });
+                return;
+            }
+
+            if (requestRoom.val().maxUsers <= Object.keys(requestRoom.val().users).length){
+                socket.emit('join-room-error', { error: 'error: joining room: room is full', message: 'The room is already full!' });
+                return;
+            }
+
+            console.log(socket.id + " UID:" + rtdbUser + " requests JOIN " + code);
+
+            await Promise.all([
+                rtdb.ref('rooms/' + code + '/users').update({
+                    [socket.id]: {
+                        userId: uid
+                    }
+                }),
+                rtdb.ref('roomUsers/' + code + '/responses').update({
+                    [uid]: {
+                        socketId: socket.id,
+                        status: 0,
+                        response: null
+                    }
+                }),
+                rtdb.ref('authUsers/' + uid).set({
+                    room: code,
+                    socketId: socket.id
+                })
+            ]);
+
+            socket.join(code);
+            socket.emit('join-room-success', {
+                roomId: code
+            });
+        }
+        catch (error) {
+            console.error(error);
         }
     });
 });
